82a78e6dc6e06cb7a991e42c4b90876ef267a85e
Brian Tomlinson <darthlukan@gmail.com>
2015-10-22 20:04:56 +0200

    finished module 1a, start of 1b

---
+++ lecture_function_design_fullspeed.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname lecture_function_design_fullspeed) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Number -> Number
+;; produce 2 times the given number
+;(define (double x) 0) ; stub
+
+(check-expect (double 3) 6) ;test
+(check-expect (double 4.2) 8.4) ;test
+
+;(define (double x)
+;  (... x))
+
+(define (double x)
+  (* 2 x))  ;eval result -> "Both tests passed!"
